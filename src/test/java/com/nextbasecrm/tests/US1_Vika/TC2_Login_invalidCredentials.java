ad2390a915b800f8839831fe070b8630d4269806
vika <victory.drumea@gmail.com>
2022-02-26 00:29:24 -0500

    TC2, TC3 done

--- src/test/java/com/nextbasecrm/tests/US1_Vika/TC2_Login_invalidCredentials.java
+++ src/test/java/com/nextbasecrm/tests/US1_Vika/TC2_Login_invalidCredentials.java
@@ -1,4 +1,55 @@
 package com.nextbasecrm.tests.US1_Vika;
 
+import com.nextbasecrm.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class TC2_Login_invalidCredentials {
+
+    WebDriver driver;
+    @BeforeMethod
+    public void SetUpMethod() {
+        //Open browser
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://login2.nextbasecrm.com/");
+    }
+
+    @Test
+    public void invalidCredentialsLogIn(){
+
+        //Enter invalid username and password
+        WebElement userName = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        userName.sendKeys("h22@cydeo.com");
+
+        WebElement password = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        password.sendKeys("User");
+
+        //click login button
+        WebElement logInBtn = driver.findElement(By.xpath("//input[@type='submit']"));
+        logInBtn.click();
+
+        //Verify incorrect login message
+        WebElement incorrectLoginMessage = driver.findElement(By.xpath("//div[@class='errortext']"));
+        String expectedMessage = "Incorrect login or password";
+        String actualMessage = incorrectLoginMessage.getText();
+
+        Assert.assertEquals(actualMessage, expectedMessage, "invalid message");
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+    }
+
 }
+
