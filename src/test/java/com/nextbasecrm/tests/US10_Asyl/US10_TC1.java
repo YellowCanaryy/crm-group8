a9103d65fc635a6dc1e0d13cf51142eb2366161d
Asyl Orozmatova <asylumaraly@gmail.com>
2022-02-27 09:43:43 -0500

    US 10 TC1 code comments updated

--- src/test/java/com/nextbasecrm/tests/US10_Asyl/US10_TC1.java
+++ src/test/java/com/nextbasecrm/tests/US10_Asyl/US10_TC1.java
@@ -23,9 +23,12 @@ public class US10_TC1 {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(ConfigurationReader.getProperty("env"));
+
+        //UserName input
         WebElement userName = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
         userName.sendKeys(ConfigurationReader.getProperty("username"));
 
+        //Password input
         WebElement password = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
         password.sendKeys("UserUser");
 
@@ -36,10 +39,14 @@ public class US10_TC1 {
 
     @Test
     public void taskCreationConfirmationMessage() throws InterruptedException {
+        //Locate "TASK" tab and click
        driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']//span")).click();
-       WebElement titleMessage = driver.findElement(By.xpath("//input[@placeholder='Things to do']"));
-        titleMessage.sendKeys("Reminder");
 
+       //Locate titlePanel and input title message
+       WebElement titleMessage = driver.findElement(By.xpath("//input[@placeholder='Things to do']"));
+       titleMessage.sendKeys("Reminder");
+
+       //Locate message body and input message
        driver.switchTo().frame(driver.findElement(By.xpath("//*[@id='bx-html-editor-iframe-cnt-lifefeed_task_form']/iframe")));
        WebElement messageBody = driver.findElement(By.xpath("//body"));
        Thread.sleep(1000);
@@ -47,9 +54,11 @@ public class US10_TC1 {
 
        driver.switchTo().defaultContent();
 
+       //Locate "SEND" button and click it
        WebElement sendButton = driver.findElement(By.xpath("//button[@id='blog-submit-button-save']"));
        sendButton.click();
 
+       //Locate message creation confirmation and compare actual one with expected
        WebElement taskCreationConfirmationMessage = driver.findElement(By.xpath("(//span[@class='feed-task-info-text-title'])[1]"));
        String expectedMessage = "Task has been created";
        String actualMessage = taskCreationConfirmationMessage.getText();
