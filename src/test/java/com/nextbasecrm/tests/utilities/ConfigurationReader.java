72d347e122421af159191ca5d6acaf01f28aea4f
jonidamuca <jonidamuca99@gmail.com>
2022-03-02 11:10:52 -0500

    US_5

--- src/test/java/com/nextbasecrm/tests/utilities/ConfigurationReader.java
+++ src/test/java/com/nextbasecrm/tests/utilities/ConfigurationReader.java
@@ -6,7 +6,6 @@ import java.util.Properties;
 
 public class ConfigurationReader {
 
-<<<<<<< HEAD
     //1- Create the object of Properties
     private static Properties properties = new Properties();
 
@@ -15,41 +14,25 @@ public class ConfigurationReader {
         try {
 
             //2- We need to open the file in java memory: FileInputStream
-=======
-    // 1- Create the object of Properties
-   private static Properties properties=new Properties();
-
-    static {
-
-        try {
-            // 2- We need to open the file in java memory: FileInputStream
->>>>>>> origin/master
             FileInputStream file = new FileInputStream("configuration.properties");
 
             //3- Load the properties object using FileInputStream object
             properties.load(file);
-<<<<<<< HEAD
 
             //close the file
             file.close();
 
 
-=======
-            // close the file
-            file.close();
->>>>>>> origin/master
         } catch (IOException e) {
             System.out.println("File not found in the ConfigurationReader class.");
             e.printStackTrace();
         }
 
     }
+
     public static String getProperty(String keyword){
         return properties.getProperty(keyword);
     }
 
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
 }
