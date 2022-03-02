24760d5942b0f0baa4d09b443923442e887e78ca
ozgul sonmez <ozgulsonmez74@gmail.com>
2022-03-02 11:05:44 -0500

    removed conf. prop. from  the Us

--- src/test/java/com/nextbasecrm/tests/US6_TC1.java
+++ src/test/java/com/nextbasecrm/tests/US6_TC1.java
@@ -1,6 +1,150 @@
 package com.nextbasecrm.tests;
+/*
+import com.nextbasecrm.tests.utilities.ConfigurationReader;
+import com.nextbasecrm.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+ */
+import java.util.concurrent.TimeUnit;
 
 public class US6_TC1 {
 
+/*
 
-}
+        public WebDriver driver;
+
+        @BeforeMethod
+        public void setUpMethod(){
+
+            driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+            driver.get(ConfigurationReader.getProperty("env"));
+
+        }
+
+        @AfterMethod
+        public void tearDownMethod(){
+
+            driver.close();
+
+        }
+
+        @Test
+        public void test1(){
+
+            WebElement driver1=driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+            driver1.sendKeys(ConfigurationReader.getProperty("username1"));
+
+            WebElement password=driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+            password.sendKeys(ConfigurationReader.getProperty("password"));
+
+            WebElement login=driver.findElement(By.xpath("//input[@value='Log In']"));
+            login.click();
+
+            WebElement menu=driver.findElement(By.xpath("//span[.='More']"));
+            menu.click();
+
+            WebElement file=driver.findElement(By.xpath("//span[@class='menu-popup-item-text']"));
+
+            Assert.assertEquals(file.getText(),"File");
+
+            WebElement appreciation=driver.findElement(By.xpath("//span[@class='menu-popup-item-text'][.='Appreciation']"));
+
+            Assert.assertEquals(appreciation.getText(),"Appreciation");
+
+            WebElement announcement=driver.findElement(By.xpath("//span[@class='menu-popup-item-text'][.='Announcement']"));
+
+            Assert.assertEquals(announcement.getText(),"Announcement");
+
+            WebElement workFlow=driver.findElement(By.xpath("//span[@class='menu-popup-item-text'][.='Workflow']"));
+
+            Assert.assertEquals(workFlow.getText(),"Workflow");
+
+        }
+
+        @Test
+        public void test2(){
+
+            WebElement driver1=driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+            driver1.sendKeys(ConfigurationReader.getProperty("username2"));
+
+            WebElement password=driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+            password.sendKeys(ConfigurationReader.getProperty("password"));
+
+            WebElement login=driver.findElement(By.xpath("//input[@value='Log In']"));
+            login.click();
+
+            WebElement menu=driver.findElement(By.xpath("//span[.='More']"));
+            menu.click();
+
+            WebElement file=driver.findElement(By.xpath("//span[@class='menu-popup-item-text']"));
+
+            Assert.assertEquals(file.getText(),"File");
+
+            WebElement appreciation=driver.findElement(By.xpath("//span[@class='menu-popup-item-text'][.='Appreciation']"));
+
+            Assert.assertEquals(appreciation.getText(),"Appreciation");
+
+            WebElement announcement=driver.findElement(By.xpath("//span[@class='menu-popup-item-text'][.='Announcement']"));
+
+            Assert.assertEquals(announcement.getText(),"Announcement");
+
+            WebElement workFlow=driver.findElement(By.xpath("//span[@class='menu-popup-item-text'][.='Workflow']"));
+
+            Assert.assertEquals(workFlow.getText(),"Workflow");
+
+
+
+
+        }
+
+
+        @Test
+        public void test3(){
+
+            WebElement driver1=driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+            driver1.sendKeys(ConfigurationReader.getProperty("username3"));
+
+            WebElement password=driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+            password.sendKeys(ConfigurationReader.getProperty("password"));
+
+            WebElement login=driver.findElement(By.xpath("//input[@value='Log In']"));
+            login.click();
+
+            WebElement menu=driver.findElement(By.xpath("//span[.='More']"));
+            menu.click();
+
+            WebElement file=driver.findElement(By.xpath("//span[@class='menu-popup-item-text']"));
+
+            Assert.assertEquals(file.getText(),"File");
+
+            WebElement appreciation=driver.findElement(By.xpath("//span[@class='menu-popup-item-text'][.='Appreciation']"));
+
+            Assert.assertEquals(appreciation.getText(),"Appreciation");
+
+            WebElement announcement=driver.findElement(By.xpath("//span[@class='menu-popup-item-text'][.='Announcement']"));
+
+            Assert.assertEquals(announcement.getText(),"Announcement");
+
+            WebElement workFlow=driver.findElement(By.xpath("//span[@class='menu-popup-item-text'][.='Workflow']"));
+
+            Assert.assertEquals(workFlow.getText(),"Workflow");
+
+
+        }
+
+ */
+
+
+    }
+
+
+
+
